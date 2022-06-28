4353386c0d5afc203537644d49938c776be2e071
unknown <niaaudita01@gmail.com>
2022-06-28 12:51:29 +0800

    Florist Gump PKW

---
+++ sweetalert.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Florist Gump!',
+    text: 'Happy Shopping!',
+    imageUrl: 'https://images.unsplash.com/photo-1452827073306-6e6e661baf57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
